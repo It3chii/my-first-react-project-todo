92bb3b32bf1bb8c9b29cfd5872b2827e38268348
It3chii <yakup.yazir@tu-dortmund.de>
2023-10-27 20:54:19 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Home from './Pages/Home'
+import Header from './Components/Header'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header/>
+      <Home/>
+    </div>
+  
+  )
+}
+
+export default App
